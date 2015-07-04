03408e9ffb2936ff5c0b2b79b8b196cc51820e19
michag86 <micha_g@arcor.de>
2015-07-04 23:16:24 +0200

    Move text to template, prepare for translation

--- passwordpolicy/templates/user.php
+++ passwordpolicy/templates/user.php
@@ -2,24 +2,24 @@
 	
 	<h2><?php p($l->t('Password Policy')); ?></h2>
 	
-	<p>The following password restrictions are currently in place:</p>
-	<p>All passwords are required to be at least <?php p($_['minlength']); ?> characters in length and;</p>
+	<p><?php p($l->t('The following password restrictions are currently in place:')); ?></p>
+	<p><?php p($l->t('All passwords are required to be at least %s characters in length and;', [ $_['minlength']])); ?></p>
 	
 	<ul style="list-style: circle; margin-left: 20px;">
 		
-		<?php if(isset($_['mixedcase'])){
+		<?php if(isset($_['mixedcase']) && $_['mixedcase'] == "true"){
 			?><li><?php
-			p($_['mixedcase']);
+			p($l->t('Must contain UPPER and lower case characters.'));
 			?></li><?php
 		}?>
-		<?php if(isset($_['numbers'])){
+		<?php if(isset($_['numbers']) && $_['numbers'] == "true"){
 			?><li><?php
-			p($_['numbers']);
+			p($l->t('Must contain numbers.'));
 			?></li><?php
 		}?>
-		<?php if(isset($_['specialcharslist'])){
+		<?php if(isset($_['specialchars']) && $_['specialchars'] == "true"){
 			?><li><?php
-			p($_['specialcharslist']);
+			p($l->t('Must contain special characters: %s', [ $_['specialcharslist'] ]));
 			?></li><?php
 		}?>
 		
