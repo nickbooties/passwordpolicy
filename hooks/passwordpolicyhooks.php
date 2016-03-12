faf82f4897e76f98a509b7a12a827886b3895698
root <root@localhost>
2016-03-12 14:22:45 +0100

    Replace static L10N

--- hooks/passwordpolicyhooks.php
+++ hooks/passwordpolicyhooks.php
@@ -2,20 +2,22 @@
 namespace OCA\PasswordPolicy\Hooks;
 use OCA\PasswordPolicy\Controller;
 use OCA\PasswordPolicy\Controller\PasswordPolicyController;
+use \OCP\IL10N;
 
 class PasswordPolicyHooks {
 
     private $userManager;
 
-    public function __construct($userManager, \OCP\IRequest $request){
+    public function __construct($userManager, \OCP\IRequest $request, IL10N $trans){
         $this->userManager = $userManager;
         $this->Request = $request;
+	$this->trans = $trans;
     }
 
     public function register() {
         $callback = function( $user, $password, $recoverPassword) {
 		// your code that executes before password is changed
-		$passwordpolicy = new PasswordPolicyController('passwordpolicy', $this->Request);
+		$passwordpolicy = new PasswordPolicyController('passwordpolicy', $this->Request, $this->trans);
 		    
 		$response = $passwordpolicy->validatepassword($password);
 		    
